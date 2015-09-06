584c3fd042f3f3a3d4d98b5dad3628d7ca5a94ee
semdinsp <scott.sproule@ficonab.com>
2015-09-06 09:12:40 +0800

    update with hurley imporovements

--- lib/wolfram_databin/base.rb
+++ lib/wolfram_databin/base.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'hurley'
 module WolframDatabin
   class Base
-  attr_accessor :shortid, :uri, :debug_flag
+  attr_accessor :shortid, :uri, :debug_flag, :clnt
   
   def set_shortid(atid)
     self.shortid=atid
@@ -19,6 +19,9 @@ module WolframDatabin
   def get_url
     url="https://datadrop.wolframcloud.com/api/v1.0/Add?"
     url
+  end
+  def get_url2
+    "https://datadrop.wolframcloud.com/api/v1.0"
   end
   def self.submit_data(tshortid,aquery)
     dbin=WolframDatabin::Base.new
@@ -39,17 +42,29 @@ module WolframDatabin
     res=self.post_data(aquery)
     res
   end
+  def build_client
+    if @clnt==nil then
+    # @clnt=HTTPClient.new 
+     @clnt=Hurley::Client.new  self.get_url2
+    end
+    @clnt
+  end
   def post_data(query)
     raise "short id not set" if self.shortid==nil
       res=''
       qdata={bin: self.shortid}.merge(self.fix_query(query))
       begin
-        Timeout::timeout(40) do    
+        timeout=40
          # @uri=URI.parse(self.get_url)
-          q=self.get_url + Hurley::Query::Flat.new(qdata).to_s
-          puts "url #{q} query #{qdata}" if self.debug_flag
-          res=Hurley.get(q)
-        end
+        # c = Hurley::Client.new "https://datadrop.wolframcloud.com/api/v1.0"
+        # c.get "Add", :time => Time.now.iso8601, :fred => "hello"
+         # q=self.get_url + Hurley::Query::Flat.new(qdata).to_s
+        #  puts "url #{q} query #{qdata}" if self.debug_flag
+         # res=Hurley.get(q)
+         @clnt=self.build_client
+         @clnt.request_options.timeout = timeout   # set to 60
+         
+         res=@clnt.get "Add", qdata
        rescue  Exception => e
          puts "Exception: #{e.inspect}"
        ensure
